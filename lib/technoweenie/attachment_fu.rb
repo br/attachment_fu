2f026e95dc95a0f10646fc00f485133e5d628768
winton <mail@wintoni.us>
2009-06-16 10:39:07 -0700

    Adding temp_file= method

--- lib/technoweenie/attachment_fu.rb
+++ lib/technoweenie/attachment_fu.rb
@@ -361,6 +361,12 @@ module Technoweenie # :nodoc:
         p = temp_paths.first
         p.respond_to?(:path) ? p.path : p.to_s
       end
+      
+      # Left out of new version for some reason.
+      def temp_path=(value)
+        temp_paths.unshift value
+        temp_path
+      end
 
       # Gets an array of the currently used temp paths.  Defaults to a copy of #full_filename.
       def temp_paths
